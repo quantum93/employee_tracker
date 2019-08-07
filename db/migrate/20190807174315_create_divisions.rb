8ac87aa20f35ba78cd468d628151ab58f8259d38
Alex Siegberg and Tae Lee <tae@>
2019-08-07 11:09:45 -0700

    This is done for one-to-multi relationship between divisions and employees.

---
+++ db/migrate/20190807174315_create_divisions.rb
@@ -0,0 +1,7 @@
+class CreateDivisions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :divisions do |t|
+      t.column(:name, :string)
+    end
+  end
+end
---
+++ db/migrate/20190807174907_create_employees.rb
@@ -0,0 +1,8 @@
+class CreateEmployees < ActiveRecord::Migration[5.2]
+  def change
+    create_table :employees do |t|
+      t.column(:name, :string)
+      t.column(:division_id, :int)
+    end
+  end
+end
