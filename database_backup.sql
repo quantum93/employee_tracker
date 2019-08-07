08a8b3557903664cc2c3bba13bbdd23c6dbbea10
Alex Siegberg and Tae Lee <tae@>
2019-08-07 16:28:06 -0700

    This is for database backup.

---
+++ database_backup.sql
@@ -0,0 +1,394 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.5
+-- Dumped by pg_dump version 10.5
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE public.ar_internal_metadata (
+    key character varying NOT NULL,
+    value character varying,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.ar_internal_metadata OWNER TO "Guest";
+
+--
+-- Name: divisions; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE public.divisions (
+    id bigint NOT NULL,
+    name character varying
+);
+
+
+ALTER TABLE public.divisions OWNER TO "Guest";
+
+--
+-- Name: divisions_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE public.divisions_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.divisions_id_seq OWNER TO "Guest";
+
+--
+-- Name: divisions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE public.divisions_id_seq OWNED BY public.divisions.id;
+
+
+--
+-- Name: employee_projects; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE public.employee_projects (
+    id bigint NOT NULL,
+    employee_id bigint,
+    project_id bigint
+);
+
+
+ALTER TABLE public.employee_projects OWNER TO "Guest";
+
+--
+-- Name: employee_projects_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE public.employee_projects_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.employee_projects_id_seq OWNER TO "Guest";
+
+--
+-- Name: employee_projects_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE public.employee_projects_id_seq OWNED BY public.employee_projects.id;
+
+
+--
+-- Name: employees; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE public.employees (
+    id bigint NOT NULL,
+    name character varying,
+    division_id integer
+);
+
+
+ALTER TABLE public.employees OWNER TO "Guest";
+
+--
+-- Name: employees_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE public.employees_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.employees_id_seq OWNER TO "Guest";
+
+--
+-- Name: employees_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE public.employees_id_seq OWNED BY public.employees.id;
+
+
+--
+-- Name: projects; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE public.projects (
+    id bigint NOT NULL,
+    name character varying
+);
+
+
+ALTER TABLE public.projects OWNER TO "Guest";
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE public.projects_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.projects_id_seq OWNER TO "Guest";
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE public.projects_id_seq OWNED BY public.projects.id;
+
+
+--
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE public.schema_migrations (
+    version character varying NOT NULL
+);
+
+
+ALTER TABLE public.schema_migrations OWNER TO "Guest";
+
+--
+-- Name: divisions id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.divisions ALTER COLUMN id SET DEFAULT nextval('public.divisions_id_seq'::regclass);
+
+
+--
+-- Name: employee_projects id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.employee_projects ALTER COLUMN id SET DEFAULT nextval('public.employee_projects_id_seq'::regclass);
+
+
+--
+-- Name: employees id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.employees ALTER COLUMN id SET DEFAULT nextval('public.employees_id_seq'::regclass);
+
+
+--
+-- Name: projects id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.projects ALTER COLUMN id SET DEFAULT nextval('public.projects_id_seq'::regclass);
+
+
+--
+-- Data for Name: ar_internal_metadata; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY public.ar_internal_metadata (key, value, created_at, updated_at) FROM stdin;
+environment	development	2019-08-07 17:44:44.191717	2019-08-07 17:44:44.191717
+\.
+
+
+--
+-- Data for Name: divisions; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY public.divisions (id, name) FROM stdin;
+4	IT
+5	Research
+6	Marketing
+3	sales
+2	Human Resource
+\.
+
+
+--
+-- Data for Name: employee_projects; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY public.employee_projects (id, employee_id, project_id) FROM stdin;
+1	1	3
+\.
+
+
+--
+-- Data for Name: employees; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY public.employees (id, name, division_id) FROM stdin;
+2	Bob	2
+3	Jake	4
+4	Josh	5
+5	Ben	2
+6	Hugh	6
+\.
+
+
+--
+-- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY public.projects (id, name) FROM stdin;
+3	Sell computer
+4	Sales Promotion
+5	Making TV Ad
+6	Sing
+2	Build website
+\.
+
+
+--
+-- Data for Name: schema_migrations; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY public.schema_migrations (version) FROM stdin;
+20190807174315
+20190807174907
+20190807175508
+20190807180131
+20190807182534
+\.
+
+
+--
+-- Name: divisions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('public.divisions_id_seq', 7, true);
+
+
+--
+-- Name: employee_projects_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('public.employee_projects_id_seq', 1, true);
+
+
+--
+-- Name: employees_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('public.employees_id_seq', 8, true);
+
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('public.projects_id_seq', 6, true);
+
+
+--
+-- Name: ar_internal_metadata ar_internal_metadata_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.ar_internal_metadata
+    ADD CONSTRAINT ar_internal_metadata_pkey PRIMARY KEY (key);
+
+
+--
+-- Name: divisions divisions_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.divisions
+    ADD CONSTRAINT divisions_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: employee_projects employee_projects_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.employee_projects
+    ADD CONSTRAINT employee_projects_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: employees employees_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.employees
+    ADD CONSTRAINT employees_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: projects projects_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.projects
+    ADD CONSTRAINT projects_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.schema_migrations
+    ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
+
+
+--
+-- Name: index_employee_projects_on_employee_id; Type: INDEX; Schema: public; Owner: Guest
+--
+
+CREATE INDEX index_employee_projects_on_employee_id ON public.employee_projects USING btree (employee_id);
+
+
+--
+-- Name: index_employee_projects_on_project_id; Type: INDEX; Schema: public; Owner: Guest
+--
+
+CREATE INDEX index_employee_projects_on_project_id ON public.employee_projects USING btree (project_id);
+
+
+--
+-- Name: employees fk_rails_550e0790c5; Type: FK CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.employees
+    ADD CONSTRAINT fk_rails_550e0790c5 FOREIGN KEY (division_id) REFERENCES public.divisions(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
